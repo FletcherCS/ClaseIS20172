01866082c16dcc11689ca9cd21a6016042e29db8
FletcherCS <kirasan_@hotmail.com>
2017-02-24 05:42:21 -0600

    modificaciones

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -23,20 +23,39 @@ public class Controlador {
     public String inicio(){
         return "inicio";
     }
+
+    @RequestMapping(value="/registro")
+    public String registro(){
+        return "registro";
+    }
     
-    @RequestMapping(value="/persona1", method = RequestMethod.GET)
+    @RequestMapping(value="registro/registrado", method = RequestMethod.GET)
     public ModelAndView persona1(ModelMap model,HttpServletRequest request){
-        String p = request.getParameter("nombre1");
-        model.addAttribute("persona", p);
-        return new ModelAndView("persona",model);
+        String p = request.getParameter("usuario");
+        String q = request.getParameter("correo");
+        String a = request.getParameter("mes");
+        String b = request.getParameter("dia");        
+        String c = request.getParameter("anio");        
+        String x = request.getParameter("correo");        
+        String y = request.getParameter("contrasena");        
+       
+        model.addAttribute("usuario", p);
+        model.addAttribute("correo", q);
+        model.addAttribute("mes", a);
+        model.addAttribute("dia", b);
+        model.addAttribute("anio", c);
+        model.addAttribute("correo", x);
+        model.addAttribute("contrasena", y);
+        return new ModelAndView("usuario",model);
     
     }
     
     @RequestMapping(value="/persona2", method = RequestMethod.POST)
     public ModelAndView persona2(ModelMap model,HttpServletRequest request){
         String p = request.getParameter("nombre2");
+        String pp = request.getParameter("contrasena");
         model.addAttribute("persona", p);
+        model.addAttribute("contrasena", pp);        
         return new ModelAndView("persona",model);
-    
     }
 }
